dfd0a9e98142a3c35c9f151084027b1d59b1a2e4
alfatah2106 <miqdad.alfatah@gmail.com>
2025-12-29 19:38:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+var GAS_API_URL = "https://script.google.com/macros/s/AKfycbyt1E66qvXcO_RuryVMLJcc_XB3lzjh2CxniaxSXViKip9Q1yZqDyp-8HsH1yayHbKO/exec";
+var GOOGLE_CLIENT_ID = "142276774829-q7b6vicbq1hm1ltng7itehietn03l119.apps.googleusercontent.com";
